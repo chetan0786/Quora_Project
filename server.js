2d360d4843cc8ad70785d08370ec9debcd5b2b0f
chetan0786 <devganchetan6@gmail.com>
2020-04-28 23:03:16 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express=require('express')
 var app=express()
 var path=require('path')
+const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
