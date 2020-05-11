b3332e4bba48ef6a25ff940ffc6ce7047b55004f
brahmjit24 <brahmjit2411@gmail.com>
2020-05-11 20:32:21 +0530

    bug fix attempt 1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.set('views', path.join(__dirname, 'Views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
