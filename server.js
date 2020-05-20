dc8db5bc4ded7afa36a535b38c549354d198ee13
chetan0786 <devganchetan6@gmail.com>
2020-05-20 23:47:43 +0530

    Added Notifications Option

--- server.js
+++ server.js
@@ -445,6 +445,53 @@ app.get('/verify',(req,res)=>
 })
 
 
+var sendNotification = function(data) {
+  var headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Basic NGEwMGZmMjItY2NkNy0xMWUzLTk5ZDUtMDAwYzI5NDBlNjJj"
+  };
+  
+  var options = {
+    host: "onesignal.com",
+    port: 443,
+    path: "/api/v1/notifications",
+    method: "POST",
+    headers: headers
+  };
+  
+  var https = require('https');
+  var req = https.request(options, function(res) {  
+    res.on('data', function(data) {
+      console.log("Response:");
+      console.log(JSON.parse(data));
+    });
+  });
+  
+  req.on('error', function(e) {
+    console.log("ERROR:");
+    console.log(e);
+  });
+  
+  req.write(JSON.stringify(data));
+  req.end();
+};
+
+var message = { 
+  app_id: "4a426ee2-1255-494f-929c-67bf4ef82e13",
+  contents: {"en": "English Message"},
+  included_segments: ["All"]
+};
+
+sendNotification(message);
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port);
